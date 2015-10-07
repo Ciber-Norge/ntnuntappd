20ffb6283e71aba993f419609d6a0fdb298af170
Ragnhild Kosmo Holm <ragnhild.kosmo.holm@Ciber.com>
2015-10-07 14:35:23 +0200

    Ragnhild made a bobo - start again - initial project

--- NTNUntappd/NTNUntappd/Properties/AssemblyInfo.cs
+++ NTNUntappd/NTNUntappd/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("62220fe1-c0cb-43d5-ae21-6c3b4612456e")]
+[assembly: Guid("64261933-fdf0-4eea-b943-888e87a53679")]
 
 // Version information for an assembly consists of the following four values:
 //
