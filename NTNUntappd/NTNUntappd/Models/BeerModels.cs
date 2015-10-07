b495a311e6740a16dd419eb5029f4ea7dd8d6e67
Ragnhild Kosmo Holm <ragnhild.kosmo.holm@Ciber.com>
2015-10-07 13:51:32 +0200

    Removed examplecode, added beermodel

---
+++ NTNUntappd/NTNUntappd/Models/BeerModels.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NTNUntappd.Models
+{
+    public class BeerModels
+    {   
+        [Key]
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Brewery { get; set; }
+    }
+}
