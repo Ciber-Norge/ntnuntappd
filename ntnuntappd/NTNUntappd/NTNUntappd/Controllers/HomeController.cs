d7335f9b86411ce13b228a8585c72e73b4244e5e
Ragnhild Kosmo Holm <ragnhild.kosmo.holm@Ciber.com>
2015-10-21 14:26:20 +0200

    SortBy most popular beer!

--- ntnuntappd/NTNUntappd/NTNUntappd/Controllers/HomeController.cs
+++ ntnuntappd/NTNUntappd/NTNUntappd/Controllers/HomeController.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Razor.Generator;
+using NTNUntappd.Models;
+
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+using NTNUntappd.Models;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using WebGrease.Css.Extensions;
+
 
 namespace NTNUntappd.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
-            return View();
+            var entries = db.CheckInModels.Include(models => models.Beer).Include(models => models.User).ToList();
+            var grouped = entries.GroupBy(models => models.Beer).OrderBy(groups => groups.Count());
+
+            var dict = grouped.ToDictionary(p => p.Key);
+            var groupedCount = dict.ToDictionary(v => v.Key, v => v.Value.Count());
+            var sorted = from entry in groupedCount orderby entry.Value descending select entry;
+            var lol = sorted.ToDictionary(pair => pair.Key, pair => pair.Value);
+           return View(lol);
         }
     }
 }
